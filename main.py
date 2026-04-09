08c8fe634b44161e8c0e6188fd7fb46104ebb075
guga <iva290386@gmail.com>
2026-04-09 23:46:59 +0400

    setup Tkinter layout with grid and entry widgets

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+password_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=password_img)
+canvas.grid(row=0, column=1)
+
+website_text = Label(text="Website: ")
+website_text.grid(row=1, column=0)
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_text = Label(text="Email/Username: ")
+email_text.grid(row=2, column=0)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+password_text = Label(text="Password: ")
+password_text.grid(row=3, column=0)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+generate_password_btn = Button(text="Generate Password")
+generate_password_btn.grid(row=3, column=2)
+
+add_btn = Button(text="Add", width=36)
+add_btn.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
