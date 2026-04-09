a38957c420eb5e24de99a9ed3b83b1070ca4298e
guga <iva290386@gmail.com>
2026-04-10 01:04:40 +0400

    implement data saving logic and validation pop-ups

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website_name = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website_name) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website_name, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
+
+        if is_ok:
+            with open("data.txt", 'a') as data_file:
+                data_file.write(f"{website_name} | {email} | {password} \n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -17,12 +34,14 @@ canvas.grid(row=0, column=1)
 website_text = Label(text="Website: ")
 website_text.grid(row=1, column=0)
 website_entry = Entry(width=35)
+website_entry.focus()
 website_entry.grid(row=1, column=1, columnspan=2)
 
 email_text = Label(text="Email/Username: ")
 email_text.grid(row=2, column=0)
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "example@gmail.com")
 
 password_text = Label(text="Password: ")
 password_text.grid(row=3, column=0)
@@ -31,7 +50,7 @@ password_entry.grid(row=3, column=1)
 generate_password_btn = Button(text="Generate Password")
 generate_password_btn.grid(row=3, column=2)
 
-add_btn = Button(text="Add", width=36)
+add_btn = Button(text="Add", width=36, command=save)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
